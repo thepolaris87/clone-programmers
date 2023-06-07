96809b16c1efbb493245d37b94dd23f4c2dfb12a
jiaezzang <visveryver2@gmail.com>
2023-06-07 14:09:23 +0900

    update

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import CodingTest from './pages/Coding';
+import Login from './pages/Login/Login';
 
 export default function Router() {
     return (
         <Routes>
             <Route path='/learn/courses' element={<CodingTest />}></Route>
+            <Route path='/learn/sign_in' element={<Login />}></Route>
         </Routes>
     );
 }
