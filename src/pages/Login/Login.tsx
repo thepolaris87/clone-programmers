1dc0f77897b1c8903ff4ce07b6347f67b2f4c3f5
jiaezzang <visveryver2@gmail.com>
2023-06-07 14:09:23 +0900

    update

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Login() {
+  return (
+    <>
+      <div className="flex text-red-500">
+        <div className="w-auto h-40 bg-blue-500 ">hello</div>
+        <div className="w-auto h-40 bg-blue-500 ">i'm</div>
+        <div className="w-auto h-40 bg-blue-500 ">header</div>
+      </div>
+      <div className="m-10 text-red-500">i'm main</div>
+    </>
+  );
+}
