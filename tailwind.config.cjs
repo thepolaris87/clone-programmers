4af196ac8394d76cb288fa6d2bb2739cde4711da
jiaezzang <visveryver2@gmail.com>
2023-06-09 08:32:35 +0900

    로그인페이지 색상 추가

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,15 @@
 module.exports = {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {}
+        extend: {
+            colors: {
+                signin_bg: "#06f",
+                signin_gray: "#f4f4f4",
+                signin_border: "#d2d2d2",
+                signin_font: "#0078ff",
+                signin_btn: "#4981f8"
+            },
+        }
     },
     plugins: []
 };
