899d63b44b332cfe8bcb4a9cfa038d528d4d46b1
jiaezzang <visveryver2@gmail.com>
2023-06-09 08:39:43 +0900

    input 공통 컴포넌트

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from "react";
 
-export default function input() {
+interface MyProps {
+  svg: any,
+  type: string,
+  placeholder: string | undefined
+}
+export default function Input(props : MyProps) {
   return (
-    <div>input</div>
-  )
+    <div className="relative flex w-full border border-signin_border h-[2.875rem] rounded-sm">
+      <div className="absolute top-3 left-3 w-fit ">
+        {props.svg}
+      </div>
+      <input
+        type={props.type}
+        className="w-full px-[2.5rem] text-[0.9rem]"
+        placeholder={props.placeholder}
+      ></input>
+    </div>
+  );
 }
