79304b9161108297fe3efb99c09185900b5700b6
jiaezzang <visveryver2@gmail.com>
2023-06-09 08:31:43 +0900

    svg 컴포넌트화

---
+++ src/assets/svg/Smile.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function Smile() {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle
+        cx="10"
+        cy="10"
+        r="9"
+        stroke="#CCC"
+        strokeMiterlimit="10"
+      ></circle>
+      <path
+        d="M6.537 11.469a5.038 5.038 0 006.64 0M6.776 7.48v1.496M13.02 7.48v1.496"
+        stroke="#CCC"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+      ></path>
+    </svg>
+  );
+}
