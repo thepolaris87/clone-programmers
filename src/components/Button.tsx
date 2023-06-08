3d9cb53c690c19f3b9b25e9e503ea4d7dff1772c
1seul357 <1seul357@gmail.com>
2023-06-08 15:14:08 +0900

    컴포넌트 UI 수정

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,6 +4,7 @@ type ButtonProps = {
     width?: string;
     height?: string;
     fontSize?: string;
+    fontWeight?: string;
     bc?: string;
     c?: string;
     mt?: string;
@@ -14,6 +15,7 @@ type ButtonProps = {
     padding?: string;
     bs?: string;
     border?: string;
+    hbc?: string;
     hc?: string;
     name: string;
 };
@@ -21,23 +23,28 @@ const StyledButton = styled.button`
     width: ${(props: ButtonProps) => (props.width ? props.width : '150px')};
     height: ${(props: ButtonProps) => (props.height ? props.height : '50px')};
     font-size: ${(props: ButtonProps) => (props.fontSize ? props.fontSize : '1rem')};
+    font-weight: ${(props: ButtonProps) => (props.fontWeight ? props.fontWeight : '600')};
     background-color: ${(props: ButtonProps) => (props.bc ? props.bc : '#44576c')};
     color: ${(props: ButtonProps) => (props.c ? props.c : 'white')};
     margin-top: ${(props: ButtonProps) => (props.mt ? props.mt : '')};
     margin-left: ${(props: ButtonProps) => (props.ml ? props.ml : '')};
     margin-bottom: ${(props: ButtonProps) => (props.mb ? props.mb : '')};
     margin-right: ${(props: ButtonProps) => (props.mr ? props.mr : '')};
-    padding: ${(props: ButtonProps) => (props.padding ? props.padding : '')};
+    padding: ${(props: ButtonProps) => (props.padding ? props.padding : '0px')};
     box-shadow: ${(props: ButtonProps) => (props.bs ? props.bs : '')};
     border: ${(props: ButtonProps) => (props.border ? props.border : 'none')};
-    font-weight: 600;
     border-radius: ${(props: ButtonProps) => (props.br ? props.br : '')};
     cursor: pointer;
     :hover {
-        background-color: ${(props: ButtonProps) => (props.hc ? props.hc : 'none')};
+        background-color: ${(props: ButtonProps) => (props.hbc ? props.hbc : 'none')};
+        color: ${(props: ButtonProps) => (props.hc ? props.hc : 'none')};
     }
 `;
 
 export const Button = (props: ButtonProps) => {
-    return <StyledButton {...props}>{props.name}</StyledButton>;
+    return (
+        <StyledButton {...props}>
+            <div className='mt-1'>{props.name}</div>
+        </StyledButton>
+    );
 };
