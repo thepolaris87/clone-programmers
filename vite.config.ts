0e80b15f016a48c4e716c0fe9a04de6631797ba5
thepolaris87 <53024147+thepolaris87@users.noreply.github.com>
2023-06-12 17:47:53 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import Markdown from '@pity/vite-plugin-react-markdown';
 import svgr from 'vite-plugin-svgr';
+// import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
