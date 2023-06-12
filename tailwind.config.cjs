d47360a44b81974f57d12ea2e58dbe615a2014fa
jiaezzang <visveryver2@gmail.com>
2023-06-12 17:58:51 +0900

    색상 추가

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,7 +9,10 @@ module.exports = {
                 signin_gray: "#f4f4f4",
                 signin_border: "#d2d2d2",
                 signin_font: "#0078ff",
-                signin_btn: "#4981f8"
+                signin_btn: "#4981f8",
+                navbar_gray1: "#aeaeae",
+                navbar_gray2: "#6b6b6b",
+                navbar_blue: "#0084ff"
             },
         }
     },
