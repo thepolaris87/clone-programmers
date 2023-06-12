ef8b84c00a58f91f2b1057f41578ad948dfb9ed7
1seul357 <1seul357@gmail.com>
2023-06-12 10:45:31 +0900

    경로 수정

--- src/components/SignIn/SocialLogin.tsx
+++ src/components/SignIn/SocialLogin.tsx
@@ -1,43 +1,31 @@
-import React from "react";
-import kakao from '../../assets/signin/kakao.png'
-import google from '../../assets/signin/google.png'
-import github from '../../assets/signin/github.png'
-import facebook from '../../assets/signin/facebook.png'
+import React from 'react';
+import kakao from '../../assets/images/signin/kakao.png';
+import google from '../../assets/images/signin/google.png';
+import github from '../../assets/images/signin/github.png';
+import facebook from '../../assets/images/signin/facebook.png';
 
 export default function SocialLogin() {
-  return (
-    <>
-      <div className="text-center mb-[1.5rem]"> 다른 계정으로 로그인 하기</div>
-      <div className="flex justify-evenly text-center text-[0.75rem]">
-        <div>
-          <img
-            src={google}
-            className="w-[2.875rem] cursor-pointer"
-          ></img>
-          <div>Google</div>
-        </div>
-        <div>
-          <img
-            src={kakao}
-            className="w-[2.875rem] cursor-pointer"
-          ></img>
-          <div>Kakao</div>
-        </div>
-        <div>
-          <img
-            src={github}
-            className="w-[2.875rem] cursor-pointer"
-          ></img>
-          <div>Github</div>
-        </div>
-        <div>
-          <img
-            src={facebook}
-            className="w-[2.875rem] cursor-pointer"
-          ></img>
-          <div>Facebook</div>
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className='text-center mb-[1.5rem]'> 다른 계정으로 로그인 하기</div>
+            <div className='flex justify-evenly text-center text-[0.75rem]'>
+                <div>
+                    <img src={google} className='w-[2.875rem] cursor-pointer'></img>
+                    <div>Google</div>
+                </div>
+                <div>
+                    <img src={kakao} className='w-[2.875rem] cursor-pointer'></img>
+                    <div>Kakao</div>
+                </div>
+                <div>
+                    <img src={github} className='w-[2.875rem] cursor-pointer'></img>
+                    <div>Github</div>
+                </div>
+                <div>
+                    <img src={facebook} className='w-[2.875rem] cursor-pointer'></img>
+                    <div>Facebook</div>
+                </div>
+            </div>
+        </>
+    );
 }
