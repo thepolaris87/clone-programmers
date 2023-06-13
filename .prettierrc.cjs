13b793dfcb1a7f501321ec112bc8e3a41646427d
김진범 <thepolaris@jei.com>
2023-06-13 13:32:05 +0900

    update

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,8 +1,8 @@
 module.exports = {
-  "singleQuote": true,
-  "trailingComma": "none",
-  "semi": true,
-  "useTabs": false,
-  "tabWidth": 4,
-  "printWidth": 120
-}
+    singleQuote: true,
+    trailingComma: 'none',
+    semi: true,
+    useTabs: false,
+    tabWidth: 4,
+    printWidth: 120
+};
