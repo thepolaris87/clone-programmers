5d06f28ead3791403f23facf424c72a1e6fdcf2e
1seul357 <1seul357@gmail.com>
2023-06-13 11:30:30 +0900

    모달 사라지게 하는 애니메이션 추가

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,27 +1,50 @@
-import { ReactNode } from 'react';
+import { ReactNode, useState, useEffect } from 'react';
 
 type ModalProps = {
     children: ReactNode;
     title: string;
     button: string;
+    open: boolean;
     onClick: (value: boolean) => void;
 };
 
-export const Modal = ({ children, title, button, onClick }: ModalProps) => {
+export const Modal = ({ children, title, button, open, onClick }: ModalProps) => {
+    const [animate, setAnimate] = useState(false);
+    const [visible, setVisible] = useState(open);
+
+    useEffect(() => {
+        setVisible(open);
+        if (visible && !open) {
+            setAnimate(true);
+            setTimeout(() => setAnimate(false), 300);
+        }
+        return () => {
+            setVisible(false);
+        };
+    }, [visible, open]);
+
+    if (!animate && !visible) return null;
     return (
-        <div className='modal-wrapper-in fixed flex justify-center items-center top-0 left-0 w-[100%] h-[100%] z-[1050] bg-[rgba(40,56,80,0.8)]'>
-            <div className='modal-content-in w-[600px] m-[40px]'>
-                <div className='w-[43.75rem] rounded-[0.25rem] bg-[white]'>
-                    <div className='h-[71px] p-[1.25rem_1.5rem_0_1.5rem]'>
-                        <button className='float-right text-[1.5rem] text-[#000] opacity-[0.16] leading-1 font-[700] hover:opacity-[0.5]' onClick={() => onClick(false)}>
+        <div
+            className={`${
+                open ? 'modal-wrapper fade' : 'modal-wrapper'
+            } fixed flex justify-center items-center top-0 left-0 w-[100%] h-[100%] z-[1050] bg-[rgba(40,56,80,0.8)]`}
+        >
+            <div className={`${open ? 'modal-content fade' : 'modal-content'} w-[600px] m-[40px]`}>
+                <div className="w-[43.75rem] rounded-[0.25rem] bg-[white]">
+                    <div className="h-[71px] p-[1.25rem_1.5rem_0_1.5rem]">
+                        <button
+                            className="float-right text-[1.5rem] text-[#000] opacity-[0.16] leading-1 font-[700] hover:opacity-[0.5]"
+                            onClick={() => onClick(false)}
+                        >
                             x
                         </button>
-                        <h3 className='text-[#263747] text-[26px] font-[700] mb-0 leading-[1.5]'>{title}</h3>
+                        <h3 className="text-[#263747] text-[26px] font-[700] mb-0 leading-[1.5]">{title}</h3>
                     </div>
-                    <div className='p-[1.5rem]'>{children}</div>
-                    <div className='p-[1.5rem] flex justify-end'>
+                    <div className="p-[1.5rem]">{children}</div>
+                    <div className="p-[1.5rem] flex justify-end">
                         <button
-                            className='bg-[#0078FF] text-[white] text-[1rem] font-[500] leading-[1.5rem] p-[0.4375rem_0.8125rem] rounded-[0.25rem]'
+                            className="bg-[#0078FF] text-[white] text-[1rem] font-[500] leading-[1.5rem] p-[0.4375rem_0.8125rem] rounded-[0.25rem]"
                             onClick={() => onClick(false)}
                         >
                             {button}
--- src/pages/Coding/CodingTest.tsx
+++ src/pages/Coding/CodingTest.tsx
@@ -45,115 +45,125 @@ export default function CodingTest() {
 
     return (
         <TestContainer>
-            <div className='codingTest block w-[100%] break-words break-keep shadow-[#172334]'>
+            <div className="codingTest block w-[100%] break-words break-keep shadow-[#172334]">
                 <Navbar setModal={setModal} />
-                <section className='h-[3.5rem] flex relative bg-[#263747] px-[1rem] justify-between shadow-[0_0.0625rem_#172334]'>
-                    <h5 className='text-[white] pt-[15px]'>귤 고르기</h5>
-                    <span className='flex items-center justify-end h-[3rem]'>
-                        <button className='hidden w-[40px] h-[24px] text-[0.75rem] text-[white] rounded-[0.2rem_0_0_0.2rem] bg-[#44576c] md:block'>
-                            <h5 className='mt-1'>dark</h5>
+                <section className="h-[3.5rem] flex relative bg-[#263747] px-[1rem] justify-between shadow-[0_0.0625rem_#172334]">
+                    <h5 className="text-[white] pt-[15px]">귤 고르기</h5>
+                    <span className="flex items-center justify-end h-[3rem]">
+                        <button className="hidden w-[40px] h-[24px] text-[0.75rem] text-[white] rounded-[0.2rem_0_0_0.2rem] bg-[#44576c] md:block">
+                            <h5 className="mt-1">dark</h5>
                         </button>
-                        <button className='hidden w-[40px] h-[24px] text-[0.75rem] text-[#44576c] hover:text-[white] bg-[#263747] hover:bg-[#44576c] rounded-[0_0.2rem_0.2rem_0] mr-[-1px] border-[#44576c] border-[1px] md:block'>
-                            <h5 className='mt-1'>light</h5>
+                        <button className="hidden w-[40px] h-[24px] text-[0.75rem] text-[#44576c] hover:text-[white] bg-[#263747] hover:bg-[#44576c] rounded-[0_0.2rem_0.2rem_0] mr-[-1px] border-[#44576c] border-[1px] md:block">
+                            <h5 className="mt-1">light</h5>
                         </button>
-                        <button className='hidden w-[59px] h-[24px] text-[0.75rem] text-[white] rounded-[0.2rem_0_0_0.2rem] bg-[#44576c] ml-[8px] md:block'>
-                            <h5 className='mt-1'>sublime</h5>
+                        <button className="hidden w-[59px] h-[24px] text-[0.75rem] text-[white] rounded-[0.2rem_0_0_0.2rem] bg-[#44576c] ml-[8px] md:block">
+                            <h5 className="mt-1">sublime</h5>
                         </button>
-                        <button className='hidden w-[34px] h-[24px] text-[0.75rem] text-[#44576c] hover:text-[white] bg-[#263747] hover:bg-[#44576c] mr-[-1px] border-[#44576c] border-[1px] md:block'>
-                            <h5 className='mt-1'>vim</h5>
+                        <button className="hidden w-[34px] h-[24px] text-[0.75rem] text-[#44576c] hover:text-[white] bg-[#263747] hover:bg-[#44576c] mr-[-1px] border-[#44576c] border-[1px] md:block">
+                            <h5 className="mt-1">vim</h5>
                         </button>
-                        <button className='hidden w-[51px] h-[24px] text-[0.75rem] text-[#44576c] hover:text-[white] bg-[#263747] hover:bg-[#44576c] rounded-[0_0.2rem_0.2rem_0] mr-[-1px] border-[#44576c] border-[1px] md:block'>
-                            <h5 className='mt-1'>emacs</h5>
+                        <button className="hidden w-[51px] h-[24px] text-[0.75rem] text-[#44576c] hover:text-[white] bg-[#263747] hover:bg-[#44576c] rounded-[0_0.2rem_0.2rem_0] mr-[-1px] border-[#44576c] border-[1px] md:block">
+                            <h5 className="mt-1">emacs</h5>
                         </button>
-                        <button className='w-[88px] h-[24px] text-[0.75rem] text-[white] rounded-[0.2rem] bg-[#44576c] ml-[8px]'>
-                            <h5 className='mt-1'>JavaScript</h5>
+                        <button className="w-[88px] h-[24px] text-[0.75rem] text-[white] rounded-[0.2rem] bg-[#44576c] ml-[8px]">
+                            <h5 className="mt-1">JavaScript</h5>
                         </button>
                     </span>
                 </section>
-                <div className='min-h-[31.25rem] h-[100%] bg-[#263747]'>
-                    <section className='flex flex-wrap h-[calc(100vh-(2.9375rem+3.5rem+3.5625rem))]'>
-                        <span className='w-[calc(40%-12px)] h-[100%] overflow-y-auto leading-7 px-[20px] py-[15px]'>
+                <div className="min-h-[31.25rem] h-[100%] bg-[#263747]">
+                    <section className="flex flex-wrap h-[calc(100vh-(2.9375rem+3.5rem+3.5625rem))]">
+                        <span className="w-[calc(40%-12px)] h-[100%] overflow-y-auto leading-7 px-[20px] py-[15px]">
                             <Q000001 />
                         </span>
-                        <div className='flex justify-end items-center w-[24px] border-r-[0.0625rem] border-[#172334]'>
-                            <img className='h-[35px] cursor-ew-resize' src={verticalButton} />
+                        <div className="flex justify-end items-center w-[24px] border-r-[0.0625rem] border-[#172334]">
+                            <img className="h-[35px] cursor-ew-resize" src={verticalButton} />
                         </div>
-                        <div className='w-[calc(60%-12px)]'>
-                            <div className='h-[calc(60%-7px)]'>
-                                <div className='h-[100%]'>
-                                    <p className='p-[16px] w-[100%] h-[54px] text-[15px] text-[white] font-[700] border-b-[1px] border-[#172334]'>
+                        <div className="w-[calc(60%-12px)]">
+                            <div className="h-[calc(60%-7px)]">
+                                <div className="h-[100%]">
+                                    <div className="p-[16px] w-[100%] h-[54px] text-[15px] text-[white] font-[700] border-b-[1px] border-[#172334]">
                                         <h5>solution.js</h5>
-                                    </p>
-                                    <div className='h-[calc(100%-54px)] w-[100%] pt-[16px]'>
+                                    </div>
+                                    <div className="h-[calc(100%-54px)] w-[100%] pt-[16px]">
                                         <Editor
-                                            language='javascript'
-                                            theme='theme'
+                                            language="javascript"
+                                            theme="theme"
                                             value={codeValue}
                                             onChange={(e) => setCodeValue(e)}
-                                            options={{ fontSize: 16, minimap: { enabled: false }, scrollbar: { vertical: 'auto', horizontal: 'auto' } }}
+                                            options={{
+                                                fontSize: 16,
+                                                minimap: { enabled: false },
+                                                scrollbar: { vertical: 'auto', horizontal: 'auto' }
+                                            }}
                                         />
                                     </div>
                                 </div>
-                                <div className='flex justify-center border-t-[1px] border-[#172334]'>
-                                    <img className='w-[35px] cursor-ns-resize' src={horizonButton} />
+                                <div className="flex justify-center border-t-[1px] border-[#172334]">
+                                    <img className="w-[35px] cursor-ns-resize" src={horizonButton} />
                                 </div>
-                                <div className='h-[calc(40%-7px)]'>
-                                    <p className='h-[41px] p-[0_16px] border-b-[1px] border-[#172334]'>
-                                        <h5 className='text-[#4F6B81] text-[14px] font-[700] pt-1'>실행 결과</h5>
-                                    </p>
-                                    <p>
-                                        <h5 className='p-[16px] text-[#78909C] text-[14px]'>실행 결과가 여기에 표시됩니다.</h5>
-                                    </p>
+                                <div className="h-[calc(40%-7px)]">
+                                    <div className="h-[41px] p-[0_16px] border-b-[1px] border-[#172334]">
+                                        <h5 className="text-[#4F6B81] text-[14px] font-[700] pt-1">실행 결과</h5>
+                                    </div>
+                                    <div>
+                                        <h5 className="p-[16px] text-[#78909C] text-[14px]">
+                                            실행 결과가 여기에 표시됩니다.
+                                        </h5>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </section>
-                    <section className='flex justify-between h-[3.5625rem] bg-[#263747] border-t-[1px] border-[#172334] px-[1rem] py-[0.5rem]'>
-                        <span className='hidden md:flex'>
-                            <button className='w-[113px] h-[40px] bg-[#44576c] hover:bg-[#37485D] text-[white] font-[600] rounded-[4px] mx-[4px]'>
-                                <h5 className='mt-1'>질문하기</h5>
+                    <section className="flex justify-between h-[3.5625rem] bg-[#263747] border-t-[1px] border-[#172334] px-[1rem] py-[0.5rem]">
+                        <span className="hidden md:flex">
+                            <button className="w-[113px] h-[40px] bg-[#44576c] hover:bg-[#37485D] text-[white] font-[600] rounded-[4px] mx-[4px]">
+                                <h5 className="mt-1">질문하기</h5>
                             </button>
-                            <button className='w-[183px] h-[40px] bg-[#44576c] hover:bg-[#37485D] text-[white] font-[600] rounded-[4px] mx-[4px]'>
-                                <h5 className='mt-1'>테스트 케이스 추가하기</h5>
+                            <button className="w-[183px] h-[40px] bg-[#44576c] hover:bg-[#37485D] text-[white] font-[600] rounded-[4px] mx-[4px]">
+                                <h5 className="mt-1">테스트 케이스 추가하기</h5>
                             </button>
                         </span>
-                        <span className='flex'>
-                            <button className='hidden w-[160px] h-[40px] bg-[#44576c] hover:bg-[#37485D] text-[white] font-[600] rounded-[4px] mx-[4px] md:block'>
-                                <h5 className='mt-1'>다른 사람의 풀이</h5>
+                        <span className="flex">
+                            <button className="hidden w-[160px] h-[40px] bg-[#44576c] hover:bg-[#37485D] text-[white] font-[600] rounded-[4px] mx-[4px] md:block">
+                                <h5 className="mt-1">다른 사람의 풀이</h5>
                             </button>
-                            <button className='w-[72px] h-[40px] bg-[#44576c] hover:bg-[#37485D] text-[white] font-[600] rounded-[4px] mx-[4px]' onClick={onReset}>
-                                <h5 className='mt-1'>초기화</h5>
+                            <button
+                                className="w-[72px] h-[40px] bg-[#44576c] hover:bg-[#37485D] text-[white] font-[600] rounded-[4px] mx-[4px]"
+                                onClick={onReset}
+                            >
+                                <h5 className="mt-1">초기화</h5>
                             </button>
-                            <button className='w-[91px] h-[40px] bg-[#44576c] hover:bg-[#37485D] text-[white] font-[600] rounded-[4px] mx-[4px]'>
-                                <h5 className='mt-1'>코드 실행</h5>
+                            <button className="w-[91px] h-[40px] bg-[#44576c] hover:bg-[#37485D] text-[white] font-[600] rounded-[4px] mx-[4px]">
+                                <h5 className="mt-1">코드 실행</h5>
                             </button>
-                            <button className='w-[140px] h-[40px] bg-[#0078ff] hover:bg-[#0053f4] text-[white] font-[600] rounded-[4px] mx-[4px]' onClick={onPostQuestion}>
-                                <h5 className='mt-1'>제출 후 채점하기</h5>
+                            <button
+                                className="w-[140px] h-[40px] bg-[#0078ff] hover:bg-[#0053f4] text-[white] font-[600] rounded-[4px] mx-[4px]"
+                                onClick={onPostQuestion}
+                            >
+                                <h5 className="mt-1">제출 후 채점하기</h5>
                             </button>
                         </span>
                     </section>
                 </div>
             </div>
-            {modal && (
-                <Modal title='컴파일 옵션' button='닫기' onClick={setModal}>
-                    <table>
-                        <thead className='text-left border-t-[1px] border-[#d7e2eb]'>
-                            <tr>
-                                <th className='w-[95px] p-[0.25rem_0.5rem]'>언어</th>
-                                <th className='w-[172px] p-[0.25rem_0.5rem]'>컴파일러</th>
-                                <th className='w-[384px] p-[0.25rem_0.5rem]'>컴파일 커맨드</th>
-                            </tr>
-                        </thead>
-                        <tbody className='bg-[rgba(50,50,144,0.02)] border-t-[1px] border-[#d7e2eb]'>
-                            <tr>
-                                <td className='w-[172px] p-[0.25rem_0.5rem]'>JavaScript</td>
-                                <td className='w-[172px] p-[0.25rem_0.5rem]'>Node.js 16.17.0</td>
-                                <td className='w-[384px] p-[0.25rem_0.5rem]'></td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </Modal>
-            )}
+            <Modal title="컴파일 옵션" button="닫기" open={modal} onClick={setModal}>
+                <table>
+                    <thead className="text-left border-t-[1px] border-[#d7e2eb]">
+                        <tr>
+                            <th className="w-[95px] p-[0.25rem_0.5rem]">언어</th>
+                            <th className="w-[172px] p-[0.25rem_0.5rem]">컴파일러</th>
+                            <th className="w-[384px] p-[0.25rem_0.5rem]">컴파일 커맨드</th>
+                        </tr>
+                    </thead>
+                    <tbody className="bg-[rgba(50,50,144,0.02)] border-t-[1px] border-[#d7e2eb]">
+                        <tr>
+                            <td className="w-[172px] p-[0.25rem_0.5rem]">JavaScript</td>
+                            <td className="w-[172px] p-[0.25rem_0.5rem]">Node.js 16.17.0</td>
+                            <td className="w-[384px] p-[0.25rem_0.5rem]"></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </Modal>
         </TestContainer>
     );
 }
