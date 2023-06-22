311f1b7b91554e8dac679a1c3be7f42a962c30e8
jiaezzang <visveryver2@gmail.com>
2023-06-22 14:38:02 +0900

    문구 변경

--- src/pages/CodingList/components/Main/Aside/Bottom.tsx
+++ src/pages/CodingList/components/Main/Aside/Bottom.tsx
@@ -1,10 +1,13 @@
+import { nameAtom } from "@/atoms/user";
 import Position from "./Position";
+import { useAtomValue } from "jotai";
 
 export default function Bottom() {
+    const name = useAtomValue(nameAtom);
     return (
         <div className="border-t border-list_border p-[1.5rem] hidden lg:block">
         <div className="flex items-center">
-            <div className="font-extrabold text-[1rem]">추천 포지션</div>
+            <div className="font-extrabold text-[1rem]">{(name) ? name + '님꼐 추천하는 포지션' : '추천 포지션'}</div>
             <div>
                 <svg width="1.25rem" className="mb-1" height="1.875rem" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M7 5L13 10.5L7 16" stroke="currentColor" strokeWidth="2"></path>
