b237850552e93f78e125f9b3890d830d46e2c9a1
1seul357 <1seul357@gmail.com>
2023-06-29 15:29:42 +0900

    모달 외부 클릭 시 모달 close

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useState, useEffect } from 'react';
+import React, { ReactNode, useState, useEffect, useRef } from 'react';
 import classNames from 'classnames';
 
 type ModalProps = {
@@ -12,6 +12,11 @@ type ModalProps = {
 export const Modal = ({ children, title, width, open, onClick }: ModalProps) => {
     const [animate, setAnimate] = useState(false);
     const [visible, setVisible] = useState(open);
+    const modalRef = useRef<HTMLDivElement | null>(null);
+
+    const onClickOut = (e: React.MouseEvent) => {
+        if (!modalRef.current?.contains(e.target as Node)) onClick(false);
+    };
 
     useEffect(() => {
         setVisible(open);
@@ -31,9 +36,10 @@ export const Modal = ({ children, title, width, open, onClick }: ModalProps) => 
                 'fixed flex justify-center top-0 w-[100%] h-[100%] z-[1050] bg-[rgba(40,56,80,0.8)] overflow-auto',
                 open ? 'modal-wrapper fade' : 'modal-wrapper'
             )}
+            onClick={onClickOut}
         >
             <div className={classNames(`sm:w-[auto] m-[40px]`, open ? 'modal-content fade' : 'modal-content')} style={{ width: width }}>
-                <div className={'rounded-[4px] bg-[white]'}>
+                <div className={'rounded-[4px] bg-[white]'} ref={modalRef}>
                     <div className="p-[16px_16px_0_16px] lg:p-[32px_40px_0] md:p-[24px_24px_0]">
                         <button
                             className="float-right text-[24px] text-[#000] opacity-[0.16] leading-1 font-[NotoSansKRBold] hover:opacity-[0.5]"
